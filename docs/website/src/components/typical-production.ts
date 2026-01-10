212d5da3373e73277c47a45f1e2d27aa7c5d654b
Elliot Shepherd <elliot@upowr.com.au>
2026-01-11 09:24:35 +1100

    more faq

--- docs/website/src/components/typical-production.ts
+++ docs/website/src/components/typical-production.ts
@@ -47,6 +47,23 @@ export class TypicalProduction extends LitElement {
         <h2>Is this ready to use in production?</h2>
         <p>Probably not. But it will get there. Give it a try, and we'll work out the kinks together.</p>
         <p><br/>Being based on typescript-go, the core compiler is in Go, which means I have to distribute binaries.<br/>Need to work out how you can trust them.</p>
+
+        <h2>TL;DR tech</h2>
+        <p>Uses <a href="https://github.com/oxc-project/tsgolint">tsgolint</a>'s shims for <a href="https://github.com/microsoft/typescript-go">typescript-go</a>'s internal packages to walk the AST and add validation functions. Deals with function params, function returns, JSON.parse, JSON.stringify, and casts.</p>
+        
+        <h2>How do I integrate this into my build?</h2>
+        <p>Either use it as a transformer in your TypeScript build step, or use the integrations for node and bun.</p>
+
+        <h2>Slow?</h2>
+        <p>Slower than not validating, faster than runtime validation libraries*.</p> 
+        <p><br/>Many optimisations still to come. It does try to validation where it can, but much more to do.</p>
+        <p><br/>There are some benchmark results in the readme (and you can run them yourself) but... lies, damn lies, and benchmarks. If you have any input on the benchmarks I'd like to hear them. <br/>This is made to be fast, if its not, thats a bug. </p>
+
+        <h2>Does this replace TypeScript?</h2>
+        <p>No. It is a companion to TypeScript that makes types enforceable at runtime. <br/>You write TypeScript. It outputs TypeScript.</p>
+      
+        <br/><br/>
+        <small>* Individual validation checks are quicker, but typical might run them a <strong>lot</strong> more often. But they are quick. So, don't know.</small>
       </div>
     `
   }
